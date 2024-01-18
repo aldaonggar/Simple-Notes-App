d70db7540eaa88c00e370ddab5cd15abb3895cc8
aldaonggar <nur.aldaonggar@student.kuleuven.be>
2024-01-18 20:42:01 +0100

    added user authentication and restrictions

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash 
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -14,18 +15,21 @@ def login():
         user = User.query.filter_by(email = email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category="s uccess")
+                login_user(user, remember=True)
+                flash('Logged in successfully!', category="success")
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category="error")
         else:
             flash('Email does not exist.', category="error")
 
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template("logout.html")
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -50,7 +54,8 @@ def sign_up():
             newUser = User(email = email, firstName = firstName, password=generate_password_hash(password1, method='scrypt'))
             db.session.add(newUser)
             db.session.commit()
+            login_user(newUser, remember=True)
             flash('success', category="success")
             return redirect(url_for('views.home'))
     
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
